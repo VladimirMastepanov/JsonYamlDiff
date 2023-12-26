d5bfe782b29d18ba8f96e987a51ce590dd0ad42b
VladimirMastepanov <rozenkranc4@gmail.com>
2023-12-26 09:50:16 +0100

    second check, rename gendiff -> index.js 1

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,12 +1,12 @@
 import { test, expect } from '@jest/globals';
 import gendiff from '../src/gendiff.js';
-import jsonStringForTest from '../__fixtures__/jsonStringForTest.js';
+// import jsonStringForTest from '../__fixtures__/jsonStringForTest.js';
 import plainStringForTest from '../__fixtures__/plainStringForTest.js';
 
 const jsonPath1 = '__fixtures__/file1.json';
 const jsonPath2 = '__fixtures__/file2.json';
-const yamlPath1 = '__fixtures__/file1.yaml';
-const ymlPath2 = '__fixtures__/file2.yml';
+// const yamlPath1 = '__fixtures__/file1.yaml';
+// const ymlPath2 = '__fixtures__/file2.yml';
 
 test('two json files, plain formatter', () => {
   expect(gendiff(jsonPath1, jsonPath2, 'plain')).toEqual(plainStringForTest);
